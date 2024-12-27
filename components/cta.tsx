22b0aeb8c279673180170286b5adbacd43522fa1
Narets Ng <narets.work@gmail.com>
2024-12-27 23:32:55 +0700

    first commit

---
+++ components/cta.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import BlurredShape from "@/public/images/blurred-shape.svg";
+
+export default function Cta() {
+  return (
+    <section className="relative overflow-hidden">
+
+    </section>
+  );
+}
